7903ee825fc121eebb0c5053d72ae5840d16157e
WuzhouDu <duwuz@outlook.com>
2024-04-22 22:45:58 +0800

    finish 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,20 @@
 const dummy = (blogs) => {
     return 1;
+};
+
+const totalLikes = (blogs) => {
+    if (!blogs) {
+        return 0;
+    }
+    else {
+        return blogs.reduce((prev, cur) => {
+            // console.log(`prev: ${prev}, cur: ${cur}`);
+            return prev + cur.likes;
+        }, 0);
+    }
 }
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
