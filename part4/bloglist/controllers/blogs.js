1ce10b815d29f6d0e928c6eeea32e3861cbbd975
WuzhouDu <duwuz@outlook.com>
2024-04-23 18:52:21 +0800

    finish 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,23 +1,24 @@
 const Blog = require('../models/blog');
+const User = require('../models/user');
 const blogsRouter = require('express').Router();
 const logger = require('../utils/logger');
 
-blogsRouter.get('/', (req, res) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            res.json(blogs)
-        });
+blogsRouter.get('/', async (req, res) => {
+    const users = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 });
+    res.json(users);
 });
 
-blogsRouter.post('/', (req, res) => {
-    const blog = new Blog(req.body)
+blogsRouter.post('/', async (req, res) => {
+    const body = req.body;
+    const anyUser = await User.findOne();;
+    body.user = anyUser._id;
+    const blog = new Blog(body);
 
-    blog
-        .save()
-        .then(result => {
-            res.status(201).json(result)
-        });
+    const savedBlog = await blog.save();
+    anyUser.blogs = anyUser.blogs.concat(savedBlog._id);
+    await anyUser.save();
+
+    res.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (req, res) => {
@@ -27,7 +28,7 @@ blogsRouter.delete('/:id', async (req, res) => {
         return res.status(200).send();
     }
     else {
-        res.status(400).send({error: "deleted content does not exist"});
+        res.status(400).send({ error: "deleted content does not exist" });
     }
 });
 
@@ -41,7 +42,7 @@ blogsRouter.put('/:id', async (req, res) => {
         likes: body.likes
     };
 
-    const updatedOne = await Blog.findByIdAndUpdate(req.params.id, blog, {new: true});
+    const updatedOne = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true });
     res.json(updatedOne);
 })
 
--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -4,19 +4,23 @@ const User = require('../models/user');
 
 userRouter.post('/', async (req, res) => {
     const { username, name, password } = req.body;
+    if (username.length >= 3 && password.length >= 3) {
+        const saltRounds = 10;
+        const passwordHash = await bcrypt.hash(password, saltRounds);
 
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(password, saltRounds);
+        const user = new User({ username, name, passwordHash });
 
-    const user = new User({username, name, passwordHash});
+        const savedUser = await user.save();
 
-    const savedUser = await user.save();
-
-    res.status(201).json(savedUser);
+        res.status(201).json(savedUser);
+    }
+    else {
+        res.status(400).json({ error: "username and password must be at least 3 characters." });
+    }
 });
 
 userRouter.get('/', async (req, res) => {
-    const users = await User.find({});
+    const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1, id: 1 });
     res.json(users);
 });
 
