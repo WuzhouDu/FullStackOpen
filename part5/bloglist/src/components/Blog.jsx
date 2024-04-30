8d23410d6cfecc0992fdf7d2e5dda25ca345e6c2
WuzhouDu <duwuz@outlook.com>
2024-04-30 16:54:45 +0800

    finish 5.7

--- part5/bloglist/src/components/Blog.jsx
+++ part5/bloglist/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>
-)
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    };
+
+    const [detail, setDetail] = useState(false);
+
+    const toggleShowDetail = () => {
+        setDetail(!detail);
+    }
+
+    return detail
+        ? (
+            <div style={blogStyle}>
+                <div>
+                    {blog.title} <button onClick={toggleShowDetail}>hide</button>
+                </div>
+                <div>
+                    {blog.url}
+                </div>
+                <div>
+                    {blog.likes} <button>like</button>
+                </div>
+                <div>
+                    {blog.author}
+                </div>
+            </div>
+        )
+        : (
+            <div style={blogStyle}>
+                <div>
+                    {blog.title} {blog.author} <button onClick={toggleShowDetail}>view</button>
+                </div>
+            </div>);
+}
 
 export default Blog
